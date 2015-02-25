c9eb146dd088f7706a01d33a36e46292fb9bab82
Blake McNeal <blake.mcneal@gmail.com>
2015-02-24 17:15:49 -0800

    Static methods, class and instance variable differences.

--- shipping.py
+++ shipping.py
@@ -1,11 +1,12 @@
 import iso6346
 
+
 class ShippingContainer():
 
     next_serial = 1337
 
     @staticmethod
-    def _make_bic_code(owner_code):
+    def _make_bic_code(owner_code, serial):
         return iso6346.create(owner_code=owner_code,
                               serial=str(serial).zfill(6))
 
@@ -16,16 +17,37 @@ class ShippingContainer():
         return result
 
     @classmethod
-    def create_empty(cls, owner_code):
-        return cls(owner_code, contents=None)
+    def create_empty(cls, owner_code, *args, **kwargs):
+        return cls(owner_code, contents=None, *args, **kwargs)
 
     @classmethod
-    def create_with_items(cls, owner_code, items):
-        return cls(owner_code, contents=list(items))
+    def create_with_items(cls, owner_code, items, *args, **kwargs):
+        return cls(owner_code, contents=list(items), *args, **kwargs)
 
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.bic = ShippingContainer._make_bic_code(
+        self.bic = self._make_bic_code(
             owner_code=owner_code,
             serial=ShippingContainer._get_next_serial())
+
+
+class RefrigeratedShippingContainer(ShippingContainer):
+
+    MAX_CELSIUS = 4.0
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(owner_code=owner_code,
+                              serial=str(serial).zfill(6),
+                              category='R')
+
+    def __init__(self, owner_code, contents, celsius):
+        super().__init__(owner_code, contents)
+        if celsius > RefrigeratedShippingContainer.MAX_CELSIUS:
+            raise ValueError("Temperature too hot!")
+        self._celsius = celsius
+
+    @property
+    def celsius(self):
+        return self._celsius
