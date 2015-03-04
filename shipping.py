d72513b430322c628abed2d011ebdc54263869d0
Blake McNeal <blake.mcneal@gmail.com>
2015-03-03 16:57:23 -0800

    Finished Property decorators

--- shipping.py
+++ shipping.py
@@ -36,18 +36,41 @@ class RefrigeratedShippingContainer(ShippingContainer):
 
     MAX_CELSIUS = 4.0
 
+    FRIDGE_VOLUME_FT3 = 100
+
     @staticmethod
     def _make_bic_code(owner_code, serial):
         return iso6346.create(owner_code=owner_code,
                               serial=str(serial).zfill(6),
                               category='R')
 
+    @staticmethod
+    def _c_to_f(celsius):
+        return celsius * 9/5 + 32
+
+    @staticmethod
+    def _f_to_c(fahrenheit):
+        return (fahrenheit - 32) * 5/9
+
     def __init__(self, owner_code, contents, celsius):
         super().__init__(owner_code, contents)
-        if celsius > RefrigeratedShippingContainer.MAX_CELSIUS:
-            raise ValueError("Temperature too hot!")
-        self._celsius = celsius
+        self.celsius = celsius
 
     @property
     def celsius(self):
         return self._celsius
+
+    @celsius.setter
+    def celsius(self, value):
+        if value > RefrigeratedShippingContainer.MAX_CELSIUS:
+            raise ValueError("Temperature too hot!")
+        self._celsius = value
+
+    @property
+    def fahrenheit(self):
+        return RefrigeratedShippingContainer._c_to_f(self.celsius)
+
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self.celsius = RefrigeratedShippingContainer._f_to_c(value)
+
